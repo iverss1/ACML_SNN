b30c241abeb97fb49352c7212f245e35cc7466d5
unknown <494006346@qq.com>
2023-04-03 12:50:33 +0900

    log message

--- SNN/datasets/__init__.py
+++
@@ -1,5 +0,0 @@
-#  Copyright (c) 2022. Yuki Tatsunami
-#  Licensed under the Apache License, Version 2.0 (the "License");
-
-from .cars import StanfordCars
-from .flowers import Flowers102
--- SNN/models/__init__.py
+++
@@ -1,3 +0,0 @@
-#  Copyright (c) 2022. Yuki Tatsunami
-#  Licensed under the Apache License, Version 2.0 (the "License");
-from .two_direction_snn import *
